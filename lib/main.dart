25b9af9e26cb687f4446308d4f362017bfe2db5e
ase21 <ase911@yandex.ru>
2021-08-29 18:00:05 +0300

    context

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,17 @@ import 'package:flutter/material.dart';
 
 int globalCount = 0;
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MySecondWidget(),
+      home: MyFirstWidget(),
+      title: 'FirstTitle',
     );
   }
 }
@@ -43,10 +32,14 @@ class MyFirstWidget extends StatelessWidget {
       ),
     );
   }
+
+  // Type returnContextRuntimeType() {
+  //   return context.runtimeType;
+  // }
 }
 
 class MySecondWidget extends StatefulWidget {
-  const MySecondWidget({ Key? key }) : super(key: key);
+  const MySecondWidget({Key? key}) : super(key: key);
 
   @override
   _MySecondWidgetState createState() => _MySecondWidgetState();
@@ -65,53 +58,9 @@ class _MySecondWidgetState extends State<MySecondWidget> {
       ),
     );
   }
-}
+  
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
+  Type returnContextRuntimeType() {
+    return context.runtimeType;
   }
 }
