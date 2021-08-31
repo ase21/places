9e60402d0e142ac46a5419ea6bbafad701b173e5
ase21 <ase911@yandex.ru>
2021-08-31 23:38:37 +0300

    создан аппбар с текстом в 2 строки, добавлен analyzer

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,6 +11,19 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 72,
+        title: Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            fontSize: 32,
+            color: Color(0xFF252849),
+            height: 1.125
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Color(0x00000000),
+      ),
       body: Center(
         child: TextField(),
       ),
