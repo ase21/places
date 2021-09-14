bc4edeb754f5fecf04608f13293edf123f3c66c7
ase21 <ase911@yandex.ru>
2021-09-14 22:53:57 +0300

    В виджете SightCard используйте ConstrainedBox, чтобы ограничить размер текста  по ширине до половины размера карточки. Посмотрите на результат. Почему размер виджета Text поменялся? После выполнения,  отрегулируйте размер текста согласно дизайну. Добавьте отступ между между фотографиями и описанием с помощью SizedBox AspectRatio используйте, чтобы привести виджеты SightCard в соотношение 3/2 Переверстать AppBar через наследника PreferredSizeWidget.

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -10,58 +10,73 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: colorCardBackgroundLightGray,
-      ),
-      margin: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-              color: colorCardBackgroundGray,
+    return AspectRatio(
+      aspectRatio: 3 / 2,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: colorCardBackgroundLightGray,
+        ),
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+                color: colorCardBackgroundGray,
+              ),
+              padding: const EdgeInsets.all(16),
+              height: 55,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    sight.type,
+                    style: const TextStyle(color: colorWhite, fontSize: 14),
+                  ),
+                  Container(
+                    color: colorGreen,
+                    height: 20,
+                    width: 20,
+                  ),
+                ],
+              ),
             ),
-            padding: const EdgeInsets.all(16),
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  sight.type,
-                  style: const TextStyle(color: colorWhite, fontSize: 14),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(12),
                 ),
-                Container(
-                  color: colorGreen,
-                  height: 20,
-                  width: 20,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(12),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    sight.name,
+                    style: const TextStyle(
+                      color: colorTextDarkGray,
+                      fontSize: 16,
+                    ),
+                  ),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 150),
+                    child: Text(
+                      sight.details,
+                      style:
+                          const TextStyle(color: colorTextGray, fontSize: 14),
+                    ),
+                  ),
+                ],
               ),
             ),
-            padding: const EdgeInsets.all(16),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                sight.name,
-                style: const TextStyle(color: colorTextDarkGray, fontSize: 16),
-              ),
-              Text(
-                sight.details,
-                style: const TextStyle(color: colorTextGray, fontSize: 14),
-              ),
-            ]),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
